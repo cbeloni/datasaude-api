a50f85df9e1deb9f8e7306cd228b66744a3c8dd6
Cauê Beloni <cbeloni@gmail.com>
2023-07-01 00:05:52 -0300

    adiconado job para coletar os poluentes a cada hora

--- app/poluente/integrations/cetesb.py
+++ app/poluente/integrations/cetesb.py
@@ -1,10 +1,24 @@
-import requests
 import asyncio
+import logging
+import requests
 
 from app.poluente.models import Poluente
 from app.poluente.repository.poluente_repository import PoluenteRepository
+from core.db import standalone_session
 
-def capa():
+
+
+def execute_get_capa():
+
+    @standalone_session
+    async def salvar(**kwargs):
+        try:
+            p: Poluente = Poluente(**kwargs)
+            await PoluenteRepository().save(p)
+        except Exception as e:
+            # Registrar o erro em um log
+            logging.exception("Erro ao salvar: ", e)
+
     url = "https://arcgis.cetesb.sp.gov.br/server/rest/services/QUALAR/CETESB_QUALAR/MapServer/6/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry={%22xmin%22:-5244191.63658331,%22ymin%22:-2739503.0937498696,%22xmax%22:-5165920.11961926,%22ymax%22:-2661231.5767858177,%22spatialReference%22:{%22wkid%22:102100}}&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100"
     response = requests.get(url)
     data = response.json()
@@ -21,19 +35,16 @@ def capa():
         poluente = feature['attributes']['POLUENTE']
         municipio = feature['attributes']['Municipio']
 
-        poluente: Poluente = Poluente(nome=nome,
-                                      situacao_rede=situacao_rede,
-                                      tipo_rede=tipo_rede,
-                                      data=data,
-                                      qualidade=qualidade,
-                                      endereco=endereco,
-                                      indice=indice,
-                                      poluente=poluente,
-                                      municipio=municipio)
 
-        asyncio.run(PoluenteRepository().save(poluente))
-        print('salvando')
-        print(poluente)
 
+        asyncio.run(salvar(nome=nome,
+                           situacao_rede=situacao_rede,
+                           tipo_rede=tipo_rede,
+                           data=data,
+                           qualidade=qualidade,
+                           endereco=endereco,
+                           indice=indice,
+                           poluente=poluente,
+                           municipio=municipio))
         poluentes.append(poluente)
     return poluentes
--- celery_task/poluente_job/poluente_job.py
+++ celery_task/poluente_job/poluente_job.py
@@ -4,16 +4,24 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from app.poluente.integrations import cetesb
 from app.poluente.models import Poluente
 from app.poluente.repository.poluente_repository import PoluenteRepository
+from core.db import standalone_session
 
 _poluenteRepository: PoluenteRepository = PoluenteRepository()
 
-def scheduled_task():
-    print("Executando tarefa agendada")
-    # poluente: Poluente = Poluente(endereco= 'Rua arujá')
-    # asyncio.run(_poluenteRepository.save(poluente))
+
+def poluente_job_task():
+    @standalone_session
+    async def salvar(**kwargs):
+        p: Poluente = Poluente(**kwargs)
+        await PoluenteRepository().save(p)
+
+    print('salvando')
+    asyncio.run(salvar(endereco='Rua arujá 2'))
+    print('salvo')
+
 
 def start():
     scheduler = BackgroundScheduler()
-    # scheduler.add_job(cetesb.capa, 'interval', minutes=0.2)
-    scheduler.add_job(scheduled_task, 'interval', minutes=2)
+    scheduler.add_job(cetesb.execute_get_capa, 'interval', minutes=30)
+    # scheduler.add_job(poluente_job_task, 'interval', minutes=0.2)
     scheduler.start()
