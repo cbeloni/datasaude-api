f07259edc2fa7704c530c4c4b7b1621a1ff67c34
Cauê Beloni <cbeloni@gmail.com>
2023-08-29 23:32:08 -0300

    add logs do configs

--- app/poluente/services/gestor_arquivos.py
+++ app/poluente/services/gestor_arquivos.py
@@ -1,13 +1,21 @@
 import boto3, os
 from dotenv import load_dotenv
 from io import BytesIO
-
+from distutils import log
 load_dotenv()
 
+aws_access_key_id = os.environ.get('aws_access_key_id')
+aws_secret_access_key = os.environ.get('aws_secret_access_key')
+endpoint_url = os.environ.get('endpoint_url')
+log.info("loading configs")
+log.info(aws_access_key_id)
+log.info(aws_secret_access_key)
+log.info(endpoint_url)
+
 _s3_client = boto3.client('s3',
-                          aws_access_key_id=os.environ.get('aws_access_key_id'),
-                          aws_secret_access_key=os.environ.get('aws_secret_access_key'),
-                          endpoint_url=os.environ.get('endpoint_url'))
+                          aws_access_key_id=aws_access_key_id,
+                          aws_secret_access_key=aws_secret_access_key,
+                          endpoint_url=endpoint_url)
 
 
 def enviar_arquivo(bucket_name: str, file: bytes, object_name: str, content_type: str, ):
