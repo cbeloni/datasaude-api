89b325da2614d84b0b7090d2a1cdb9354c0f50b8
Cauê Beloni <cbeloni@gmail.com>
2023-11-19 22:53:27 -0300

    add pi id

--- app/paciente/services/paciente_service.py
+++ app/paciente/services/paciente_service.py
@@ -1,7 +1,7 @@
 from core.db.session import session
 def query_pacientes():
     return """   
-           SELECT p.cd_atendimento, p.nm_paciente, pi.data, pc.endereco, pc.longitude, pc.latitude, pc.x, pc.y, pi.indice_interpolado as indice, pi.poluente
+           SELECT p.cd_atendimento, p.nm_paciente, pi.id, pi.data, pc.endereco, pc.longitude, pc.latitude, pc.x, pc.y, pi.indice_interpolado as indice, pi.poluente
           FROM paciente p, paciente_coordenadas pc, paciente_interpolacao pi
          WHERE p.id = pc.id_paciente
            AND pc.id = pi.id_coordenada
