47f596d1924dba6035b593014fafcf952791e261
caue beloni <cbeloni@gmail.com>
2024-02-21 22:34:41 -0300

    fix hostname redis

--- celery_task/__init__.py
+++ celery_task/__init__.py
@@ -2,7 +2,7 @@ import time
 
 from celery import Celery
 
-celery_app = Celery('tasks', broker="redis://localhost:6379")
+celery_app = Celery('tasks', broker="redis://redis:6379")
 
 @celery_app.task(name="paciente_task")
 def paciente_task(task_type):
