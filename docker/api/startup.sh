4e687e88cd33932fd73ddf467709178c04d93388
caue beloni <cbeloni@gmail.com>
2024-02-22 23:12:43 -0300

    fix celery entrypoint

--- docker/api/startup.sh
+++ docker/api/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dockerize -wait tcp://db:3306 -timeout 120s
-nohup celery -A celery_task worker --loglevel=info && > celery.log &&
-nohup celery -A celery_task flower --port=5001 && > celery_flower.log &&
+celery -A celery_task worker --loglevel=info & > celery.log &
+celery -A celery_task flower --port=5001 & > celery_flower.log &
 alembic upgrade head && gunicorn --bind 0.0.0.0:8000 -w 4 -k uvicorn.workers.UvicornWorker app.server:app --log-file=datasaude.log --log-level=debug
