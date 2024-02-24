28d7a4934b09b95d5fe3b2a5f08e0beaa001a8ef
caue beloni <cbeloni@gmail.com>
2024-02-23 22:09:09 -0300

    diminuido threads

--- docker/api/startup.sh
+++ docker/api/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dockerize -wait tcp://db:3306 -timeout 120s
-celery -A celery_task worker --loglevel=info & > celery.log &
+celery -A celery_task worker --concurrency=1 --loglevel=info & > celery.log &
 celery -A celery_task flower --port=5001 & > celery_flower.log &
 alembic upgrade head && gunicorn --bind 0.0.0.0:8000 -w 4 -k uvicorn.workers.UvicornWorker app.server:app --log-file=datasaude.log --log-level=debug
