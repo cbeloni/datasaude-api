2a18c43606da466feca24af3716c3b95a5b706e3
caue beloni <cbeloni@gmail.com>
2024-02-24 12:57:38 -0300

    adicionado rabbitmq

--- docker/api/startup.sh
+++ docker/api/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dockerize -wait tcp://db:3306 -timeout 120s
-celery -A celery_task worker --concurrency=1 --loglevel=info & > celery.log &
-celery -A celery_task flower --port=5001 & > celery_flower.log &
+python3 listeners/paciente_listener.py > paciente_listener.log &
+python3 listeners/geolocalizacao_listener.py > geolocalizacao_listener.log &
 alembic upgrade head && gunicorn --bind 0.0.0.0:8000 -w 4 -k uvicorn.workers.UvicornWorker app.server:app --log-file=datasaude.log --log-level=debug
