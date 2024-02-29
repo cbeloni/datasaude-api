e2e85aa8fead85e53a7f6ec330131bcc5502cfb2
caue beloni <cbeloni@gmail.com>
2024-02-28 23:26:04 -0300

    fix ack message

--- listeners/paciente_listener.py
+++ listeners/paciente_listener.py
@@ -17,6 +17,7 @@ log = logging.getLogger(__name__)
 
 
 async def on_message(message: IncomingMessage):
+    payload = None
     try:
         log.info("Processamento mensagem {}".format(message.body))
         body = message.body.decode("utf-8")
@@ -24,13 +25,20 @@ async def on_message(message: IncomingMessage):
         paciente = PacienteBase(**payload)
         result = await paciente_salvar(paciente.json())
         if (result.status_code != 200):
-            payload_deadletter = PacienteTaskError(**payload)
-            payload_deadletter.error = result.content
-            await send_rabbitmq(payload_deadletter.to_message(), "paciente_deadletter")
+            await send_deadletter(payload, result.content)
         log.info(f"Fim mensagem {message.body} - resultado {result}")
         await message.ack()
     except Exception as e:
         log.error(f"Erro no processamento da mensagem: {e}")
+        await send_deadletter (payload, e)
+        await message.ack()
+
+
+async def send_deadletter(payload, error_message):
+    payload_deadletter = PacienteTaskError(**payload)
+    payload_deadletter.error = error_message
+    await send_rabbitmq(payload_deadletter.to_message(), "paciente_deadletter")
+
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
