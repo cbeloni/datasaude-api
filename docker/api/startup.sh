e4fd8ec4709935a0daa18aa0f9b979a1d1935805
caue beloni <cbeloni@gmail.com>
2024-03-05 23:24:56 -0300

    add processa coordenadas por id listener

--- docker/api/startup.sh
+++ docker/api/startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 dockerize -wait tcp://db:3306 -timeout 120s
-python3 run_worker.py > paciente_listener.log &
-python3 run_worker.py > geolocalizacao_listener.log &
+python3 run_worker.py > listeners.log &
 alembic upgrade head && gunicorn --bind 0.0.0.0:8000 -w 4 -k uvicorn.workers.UvicornWorker app.server:app --log-file=datasaude.log --log-level=debug
--- run_worker.py
+++ run_worker.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 from listeners.config import inicialize
 from listeners.paciente_listener import on_message as on_message_paciente
+from listeners.geolocalizacao_listener import on_message as on_message_geolocalizacao
 
 log = logging.getLogger(__name__)
 
@@ -9,4 +10,5 @@ if __name__ == "__main__":
     log.info("Inicializando listener paciente")
     loop = asyncio.get_event_loop()
     loop.create_task(inicialize(loop, "paciente_upsert", on_message_paciente))
+    loop.create_task(inicialize(loop, "geolocalizacao_upsert", on_message_geolocalizacao))
     loop.run_forever()
