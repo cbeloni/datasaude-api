d335018b91d97030fa3f4bd4bdd0631e7f659273
caue beloni <cbeloni@gmail.com>
2024-03-05 23:06:39 -0300

    add processa coordenadas por id listener

--- docker/api/startup.sh
+++ docker/api/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 dockerize -wait tcp://db:3306 -timeout 120s
 python3 run_worker.py > paciente_listener.log &
+python3 run_worker.py > geolocalizacao_listener.log &
 alembic upgrade head && gunicorn --bind 0.0.0.0:8000 -w 4 -k uvicorn.workers.UvicornWorker app.server:app --log-file=datasaude.log --log-level=debug
