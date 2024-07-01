040a858bf09b0e98c0037063ca68e1c66c1dba5a
caue beloni <cbeloni@gmail.com>
2024-07-01 00:06:22 -0300

    ajuste startup

--- startup.sh
+++ startup.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-dockerize -wait tcp://db:3306 -timeout 120s
 alembic upgrade head && gunicorn --bind 0.0.0.0:8000 -w 4 -k uvicorn.workers.UvicornWorker app.server:app --log-file=datasaude.log --log-level=debug
