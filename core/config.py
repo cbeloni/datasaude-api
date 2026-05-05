36f17c695c338b9aceafcbcd188735ff632dd744
Cauê Beloni <cbeloni@gmail.com>
2026-05-04 22:58:05 -0300

    port redis

--- core/config.py
+++ core/config.py
@@ -22,7 +22,7 @@ class Config(BaseSettings):
     CELERY_BROKER_URL: str = "redis://localhost:6379/0"
     CELERY_BACKEND_URL: str = "redis://localhost:6379/0"
     REDIS_HOST: str = "localhost"
-    REDIS_PORT: int = 6379
+    REDIS_PORT: str = "6379"
 
 
 class DevelopmentConfig(Config):
